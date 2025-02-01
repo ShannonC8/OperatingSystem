f4fbb5897c0c2713ec623423161179c4ec928c97
Shannon Cheng <shannoncheng08.com>
2025-02-01 13:00:53 -0500

    comments

--- src/main.asm
+++ src/main.asm
@@ -14,25 +14,31 @@ puts:
     ;saves registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb ; loads next char in al
+    lodsb ; Load next character from [SI] into AL
     or al, al ; verify if next char is null
     jz .done ; jump zero
     
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
+    mov ah, 0x0e ; BIOS teletype output function
+    mov bh, 0   ; Page number (always 0)
+    int 0x10 ; BIOS interrupt to print character in AL
     jmp .loop
     
 
 .done:
+    pop bx ; store popped item in bx (og bx)
     pop ax
     pop si
     ret
 
 main:
-    ; setup data segments
+    ; setup data segmentsi
+    ; The segment registers (DS, ES) define memory regions for data access.
+    ; Cannot modify DS or ES directly; load the value into AX first, then copy it to DS/ES.
+    ; all memory addresses in our code will be relative to 0x0000:7C00.
+
     mov ax, 0 ; can't write to ds/es directly
     mov ds, ax
     mov es, ax
